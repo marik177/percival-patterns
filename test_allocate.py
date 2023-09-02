111711011a838c3af409ae7a722c06ffdbbb7710
Roman K <marik177@gmail.com>
2023-09-02 13:46:17 +0300

    add another test for allocate

--- test_allocate.py
+++ test_allocate.py
@@ -7,10 +7,23 @@ later = today + timedelta(days=10)
 
 
 def test_prefers_current_stock_batches_to_shipments():
-    in_stock_batch = Batch("in-warehouse-stock", "RETRO-CLOCK", 100, eta=None)
+    in_stock_batch = Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
     shipment_batch = Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
     line = OrderLine("oref", "RETRO-CLOCK", 10)
 
     allocate(line, [in_stock_batch, shipment_batch])
     assert in_stock_batch.available_quantity == 90
     assert shipment_batch.available_quantity == 100
+
+
+def test_prefers_earlier_batches():
+    earliest = Batch("speedy-batch", "MINIMALIST-SPOON", 100, eta=today)
+    medium = Batch("normal-batch", "MINIMALIST-SPOON", 100, eta=tomorrow)
+    latest = Batch("slow-batch", "MINIMALIST-SPOON", 100, eta=later)
+    line = OrderLine("order1", "MINIMALIST-SPOON", 10)
+
+    allocate(line, [medium, earliest, latest])
+
+    assert earliest.available_quantity == 90
+    assert medium.available_quantity == 100
+    assert latest.available_quantity == 100

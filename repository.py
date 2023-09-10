3e4b89aab21cd467902ae0d736a26f97c3880383
Roman K <marik177@gmail.com>
2023-09-10 16:01:13 +0300

    add repository for raw sql

--- repository.py
+++ repository.py
@@ -1,5 +1,7 @@
 import abc
-from sqlalchemy.orm import Session
+
+import sqlalchemy
+
 import model
 
 
@@ -14,18 +16,44 @@ class AbstractRepository(abc.ABC):
 
 
 class SqlRepository(AbstractRepository):
-    def __init__(self, session: Session):
+    def __init__(self, session):
         self.session = session
 
     def get(self, reference):
-        pass
+        batch = self.session.execute(
+            "SELECT reference, sku, _purchased_quantity, eta FROM batches "
+            "WHERE reference=:ref",
+            dict(ref=reference)
+        ).fetchone()
+        batch = model.Batch(*batch)
+        orderlines_in_batch = (get_allocations(self.session, batch.reference))
+        allocations = []
+        for order_id in orderlines_in_batch:
+            allocations.append(self.session.execute(
+                "SELECT orderid, sku, qty FROM order_lines WHERE orderid=:order_id", dict(order_id=order_id)).fetchone(),
+            )
+        for orerline in allocations:
+            batch._allocations.add(model.OrderLine(*orerline))
+        return batch
 
     def add(self, batch: model.Batch):
-        pass
+        self.session.execute(
+            "INSERT INTO batches (reference, sku, _purchased_quantity, eta) VALUES"
+            "(:reference, :sku, :purchased_quantity, :eta)",
+            dict(reference=batch.reference, sku=batch.sku,
+                 purchased_quantity=batch._purchased_quantity, eta=batch.eta)
+        )
 
 
-
-
-
-
-
+def get_allocations(session, batchid):
+    rows = list(
+        session.execute(
+            "SELECT orderid"
+            " FROM allocations"
+            " JOIN order_lines ON allocations.orderline_id = order_lines.id"
+            " JOIN batches ON allocations.batch_id = batches.id"
+            " WHERE batches.reference = :batchid",
+            dict(batchid=batchid),
+        )
+    )
+    return {row[0] for row in rows}

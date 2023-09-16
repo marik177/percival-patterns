9943c7919778a7a3d6490fa4f44efe5ce03994f4
Roman K <marik177@gmail.com>
2023-09-16 17:59:49 +0300

    FakeRepository [fake_repo]

--- test_services.py
+++ test_services.py
@@ -2,6 +2,20 @@ import model
 import services
 
 
+class FakeRepository(repository.AbstractRepository):
+    def __init__(self, batches):
+        self._batches = set(batches)
+
+    def add(self, batch):
+        self._batches.add(batch)
+
+    def get(self, reference):
+        return next(b for b in self._batches if b.reference == reference)
+
+    def list(self):
+        return list(self._batches)
+
+
 def test_returns_allocation():
     line = model.OrderLine("o1", "COMPLICATED-LAMP", 10)
     batch = model.Batch("b1", "COMPLICATED-LAMP", 100, eta=None)

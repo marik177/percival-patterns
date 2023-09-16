47d83bc4ee215360b0697dc8dd05a8ccf6f6f998
Roman K <marik177@gmail.com>
2023-09-16 17:55:20 +0300

    first tests for the services layer [first_services_tests]

---
+++ test_services.py
@@ -0,0 +1,11 @@
+import model
+import services
+
+
+def test_returns_allocation():
+    line = model.OrderLine("o1", "COMPLICATED-LAMP", 10)
+    batch = model.Batch("b1", "COMPLICATED-LAMP", 100, eta=None)
+    repo = FakeRepository([batch])
+
+    result = services.allocate(line, repo, FakeSession())
+    assert result == "b1"

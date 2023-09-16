b663878e2f818121dbccd64d091af3fe018282b3
Roman K <marik177@gmail.com>
2023-09-16 18:13:10 +0300

    test commmits [second_services_test]

--- test_services.py
+++ test_services.py
@@ -1,5 +1,7 @@
 import model
 import services
+import repository
+import pytest
 
 
 class FakeRepository(repository.AbstractRepository):
@@ -30,3 +32,23 @@ def test_returns_allocation():
 
     result = services.allocate(line, repo, FakeSession())
     assert result == "b1"
+
+
+def test_error_for_invalid_sku():
+    line = model.OrderLine("o1", "NONEXISTENTSKU", 10)
+    batch = model.Batch("b1", "AREALSKU", 100, eta=None)
+    repo = FakeRepository([batch])
+
+    with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
+        services.allocate(line, repo, FakeSession())
+
+
+def test_commits():
+    line = model.OrderLine("o1", "COMPLICATED-LAMP", 10)
+    batch = model.Batch("b1", "COMPLICATED-LAMP", 100, eta=None)
+    repo = FakeRepository([batch])
+    session = FakeSession()
+
+    services.allocate(line, repo, session)
+    assert session.committed is True
+
